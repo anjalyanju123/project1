cc20eb0696500a8e0a50588ca010a635d8061073
Anjaly Biju <133839733+anjalyanju123@users.noreply.github.com>
2023-11-30 09:07:34 +0530

    Add files via upload

---
+++ anjaly6/projt3/app3/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from app3.models import *
+from app3.form import *
+
+# Create your views here.
+def base(request):
+    return render(request,'base.html')
+def upload(request):
+    form=bookform()
+    if(request.method=='POST'):
+        form=bookform(request.POST,request.FILES)
+        if(form.is_valid()):
+            form.save()
+            return base(request)
+    return render(request,'add_book.html',{'form':form})   
+def booklist(request):
+    b=Book.objects.all()
+    return  render(request,'list.html',{'b':b})
+
+def edit_item(request,p):
+   b=Book.objects.get(pk=p)
+   form=bookform(instance=b)
+   if (request.method=='POST'):
+       form=bookform(request.POST,request.FILES,instance=b)
+       if form.is_valid():
+            form.save()
+            return base(request)
+   return render(request,'edit.html',{'form':form}) 
